0a6ab2bfcef1df3a380a7f260095daa1d67a91c9
Henry <hennyhux@gmail.com>
2021-12-28 14:58:00 -0500

    Add project files.

---
+++ Strings/StringEasyQuestions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewQuestions.Strings
+{
+    public static class StringEasyQuestions
+    {
+        //Given a string, create a new string without vowels and print that string.
+        public static String RemoveVowel(String inputStr)
+        {
+
+            char[] vowels = { 'a', 'e' , 'i' , 'o' ,'u' , 'A', 'E', 'I', 'O', 'U' };
+
+            return null;
+        }
+    }
+}
